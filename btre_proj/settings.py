82a10d4c5712220226faaefc145da7cc2886be87
Ahnaaf Al Rafee <ahnaafarafee@gmail.com>
2020-06-20 16:54:23 +0600

    remove postgres and added sqllite

--- btre_proj/settings.py
+++ btre_proj/settings.py
@@ -84,14 +84,10 @@ WSGI_APPLICATION = 'btre_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'btre_db',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
